ddc0764fdb3534f058df0999bb5e0771989efa42
rynhan <hanzr250@gmail.com>
2024-01-27 11:37:45 +0700

    Seventh commit. Chapter 16: Adding Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,26 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-three-roan-90.vercel.app/'),
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* <title>Acme</title> */}
+        {/* <meta name="description" content="A brief description of the page content." /> */}
+        {/* <meta name="keywords" content="keyword1, keyword2, keyword3" /> */}
+        <meta property="og:title" content="Acme (title)" />
+        <meta property="og:url" content="https://nextjs-dashboard-three-roan-90.vercel.app/" />
+        <meta property="og:description" content="Acme Dashboard (description)" />
+        <meta property="og:image" content="../public/opengraph-image.png" />
+        <link rel="icon" href="path/to/favicon.ico" />
+      </head>
       <body className={`${inter.className} antialiased`}> {children} </body>
     </html>
   );
