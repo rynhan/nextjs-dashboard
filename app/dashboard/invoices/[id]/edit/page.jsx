1281423666021a544023166e27e1a37eb8cc8d02
rynhan <hanzr250@gmail.com>
2024-01-27 10:16:04 +0700

    Sixth commit. Chapter 13: Handling Error

--- app/dashboard/invoices/[id]/edit/page.jsx
+++ app/dashboard/invoices/[id]/edit/page.jsx
@@ -1,7 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
- 
+import { notFound } from 'next/navigation';
+
 export default async function Page({ params }) {
   
   const id = params.id;
@@ -9,6 +10,10 @@ export default async function Page({ params }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
